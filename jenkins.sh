0a9d0f2462533b683147f5ce4c77ad699956a0bf
vamshikira <89897392+vamshikira@users.noreply.github.com>
2023-09-26 00:26:26 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 sudo apt update -y
-
 sudo apt upgrade -y 
-
 sudo apt install openjdk-17-jre -y
-
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
